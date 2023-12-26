f4897c732b6a72d313b010560450383d1175c4f2
KitwanaSh <rodriguekitweze@gmail.com>
2023-12-26 10:52:49 +0000

    Feat: Add the room model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    """ The room model representation """
+    #host =
+    #topi =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """ Return string representation of room name """
+        return self.name
