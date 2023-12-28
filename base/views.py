99f0c894360d2289589d5cee863ffe208bfd9453
KitwanaSh <rodriguekitweze@gmail.com>
2023-12-28 14:36:44 +0000

    Fix: Ignore pyc files

--- base/views.py
+++ base/views.py
@@ -33,3 +33,17 @@ def createRoom(request):
 
     context = {"form": form}
     return render(request, "base/room_form.html", context)
+
+def updateRoom(request, pk):
+    """ Update room for a particular user """
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
