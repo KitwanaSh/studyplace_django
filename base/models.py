b4c9c67ccd3cae2c04a1d4c88fd8c3ad04d9f2df
KitwanaSh <rodriguekitweze@gmail.com>
2023-12-28 13:45:45 +0000

    Feat: Update the model

--- base/models.py
+++ base/models.py
@@ -16,10 +16,14 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         """ Return string representation of room name """
