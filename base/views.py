9a52b0bffc62eb75ed3dac26f9c293d77cf1e5d3
KitwanaSh <rodriguekitweze@gmail.com>
2023-12-31 13:28:34 +0000

    Feat: Add activities on home page

--- base/views.py
+++ base/views.py
@@ -71,11 +71,13 @@ def home(request):
     topics = Topic.objects.all()
 
     room_count = rooms.count()
+    room_messages = Message.objects.all()
 
     context = {
         'rooms': rooms,
         'topics': topics,
-        'room_count': room_count
+        'room_count': room_count,
+        'room_messages': room_messages
         }
 
     return render(request, "base/home.html", context)
@@ -83,7 +85,7 @@ def home(request):
 def room(request, pk):
     """ The room page """
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
 
     if request.method == "POST":
