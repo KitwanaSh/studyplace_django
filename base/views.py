c484c3dca468614f2302ba1bc49986cee7632989
KitwanaSh <rodriguekitweze@gmail.com>
2024-01-03 17:53:36 +0000

    Feat: Update

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 def loginPage(request):
     """ Login page view """
@@ -188,3 +188,16 @@ def deleteMessage(request, pk):
         return  redirect('home')
     
     return render(request, "base/delete.html", {"obj":message})
+
+@login_required(login_url="login-user")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm( request.POST, instance=user)
+        if form.is_valid:
+            form.save()
+            return redirect("user-profile", pk=user.id)
+        
+    return render(request, "base/update_user.html", {"form": form})
