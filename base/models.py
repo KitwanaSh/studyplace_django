7a168094ab09d13323b8b3359b982af9e20224f8
KitwanaSh <rodriguekitweze@gmail.com>
2024-01-07 15:10:20 +0000

    Feat: Update elements in the user model

--- base/models.py
+++ base/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True)
+    bio = models.TextField(null=True)
+
+    # avatar = 
 
 class Topic(models.Model):
     """ Return the topic here """
